07fd9d4aceca154fd538a70266a3a31436b10f3f
visalrotanak <visalrotanak7@gmail.com>
2022-10-24 13:48:26 +0700

    main.py

---
+++ run.bat
@@ -0,0 +1,1 @@
+cd "D:/chor_visalrotanak/github_repository/Daily-Git-Commit" & "C:/Users/dvann/AppData/Local/Programs/Python/Python310/python.exe" "D:/chor_visalrotanak/github_repository/Daily-Git-Commit/main.py"
